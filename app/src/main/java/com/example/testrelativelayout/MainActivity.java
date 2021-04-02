389085fab0bdfcab94e23b9852728952b8f3b5f7
Mycom <kiw970923@naver.com>
2021-04-02 21:00:15 +0900

    이미지버튼 터치 시 이미지가 바뀌고 손을 떼면 원상복귀

--- app/src/main/java/com/example/testrelativelayout/MainActivity.java
+++ app/src/main/java/com/example/testrelativelayout/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.testrelativelayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton imgbtn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgbtn1=(ImageButton)findViewById(R.id.ImgBtn1);
+
+        imgbtn1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction()==MotionEvent.ACTION_DOWN)
+                    imgbtn1.setBackgroundResource(R.drawable.touched);
+                else if(motionEvent.getAction()==MotionEvent.ACTION_UP)
+                    imgbtn1.setBackgroundResource(R.drawable.untouched);
+                return false;
+            }
+        });
     }
 }
